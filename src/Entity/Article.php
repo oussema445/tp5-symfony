1e0d61efad839a1fb65e07585ce06948b031a7ef
oussema Mhamed <120645606+oussema445@users.noreply.github.com>
2024-05-02 20:07:07 +0200

    tp5

---
+++ src/Entity/Article.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 50,
+     *      minMessage = "Le nom d'un article doit comporter au moins {{ limit }} caractères",
+     *      maxMessage = "Le nom d'un article doit comporter au plus {{ limit }} caractères"
+     * )
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=0)
+     * @Assert\NotEqualTo(
+     *      value = 0,
+     *      message = "Le prix d’un article ne doit pas être égal à 0 "
+     * )
+     */
+    private $prix;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    public function setPrix($prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
